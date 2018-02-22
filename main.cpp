9fe0e62cdbe4af5d90936c48656555e851de8090
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-21 18:14:26 -0800

    Repeats menu or quits.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
+//Author: Tanya Medina-Mercado
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  string list[5];
   int numItems = 0;
   char input;
 
@@ -14,6 +15,17 @@ int main()
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  if (input == 'q' || input == 'Q')
+  cout<<"Thanks,\n";
+  else
+  {
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/Q): ";
+  cin>>input;
+  }
 
-  return 0;
+return 0;
 }
