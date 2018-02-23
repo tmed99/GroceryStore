61330405e4df3221d8199a0531a1b470bd8d15d6
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-22 23:42:53 -0800

    Makes a list.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,11 @@ int main()
   cin>>input;
   if (input == 'a' || input == 'A')
   {
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/Q): ";
   cout<<"What is the item?";
   cin>>item;
   list[0];
